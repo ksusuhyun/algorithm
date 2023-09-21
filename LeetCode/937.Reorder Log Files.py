efab17e43efe1fe37e49e117db691d7c26d8580c
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2023-09-22 02:15:31 +0900

    Add files via upload

---
+++ LeetCode/937.Reorder Log Files.py
@@ -0,0 +1,13 @@
+#leet code 937.Reorder Log Files
+
+def solution(logs):
+    letters, digits = [], []
+    for log in logs:
+        if log.split()[1].isdight():
+            digits.append(log)
+        else:
+            letters.append(log)
+    
+    letters.sort(key = lambda x : (x.split()[1:], x.split()[0]))
+    
+    return letters+digits
