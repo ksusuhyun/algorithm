176637cd6d6e7e2d2411bfb082941b0d96cd9f75
kimsuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-09-24 20:39:41 +0900

    [Silver V] Title: 영화감독 숌, Time: 732 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -4,9 +4,7 @@ num = 666
 
 while cnt != target:
     num += 1
-    str_num = str(num)
-    for i in range(0,len(str_num)-2):
-        if str_num[i:i+3] == '666':
-            cnt += 1
-            break
+    if '666' in str(num):
+        cnt += 1
+        
 print(num)
