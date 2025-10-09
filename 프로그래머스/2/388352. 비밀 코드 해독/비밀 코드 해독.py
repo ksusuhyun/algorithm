c0f8108abdeb91a0d4d5b13250e188548552e87d
kimsuhyun <100765937+ksusuhyun@users.noreply.github.com>
2025-10-09 16:26:32 +0900

    [level 2] Title: 비밀 코드 해독, Time: 455.25 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -10,20 +10,8 @@ def solution(n, q, ans):
             com = set(com)
             set_input = set(input)
             
-            if len(list(com & set_input)) >= ans[idx]:
-                new_all_com.append(com)
-        all_com = new_all_com
-        
-    # 마지막으로 모든 입력한 정수와의 교집합이 같지 않으면 제거한다
-    for idx, input in enumerate(q):
-        new_all_com = []
-        for com in all_com:
-            com = set(com)
-            set_input = set(input)
-            
             if len(list(com & set_input)) == ans[idx]:
                 new_all_com.append(com)
         all_com = new_all_com
         
-    # print(new_all_com)
     return len(new_all_com)
