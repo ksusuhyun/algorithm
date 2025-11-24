9a0bbb36d370eb57dfc82372476774543a51114e
kimsuhyun <100765937+ksusuhyun@users.noreply.github.com>
2025-11-24 16:41:58 +0900

    [Silver I] Title: 미로 탐색, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -12,18 +12,22 @@ def check_in(x, y):
     if 0<=x<N and 0<=y<M:
         return True
 
-def bfs():
+def bfs(sx, sy, ex, ey):
     q = []
-    q.append((0,0))
-    v[0][0] = 1
+    q.append((sx,sy))
+    v[sx][sy] = 1
 
     while q:
         c_x, c_y = q.pop(0)
+
+        # 정답 조건 처리
+        if (c_x, c_y) == (ex, ey):
+            return v[c_x][c_y]
+
         for d_x, d_y in d:
             new_x, new_y = c_x + d_x, c_y + d_y
             if check_in(new_x, new_y) and arr[new_x][new_y] and not v[new_x][new_y]:
                 q.append((new_x, new_y))
                 v[new_x][new_y] = v[c_x][c_y] + 1
 
-bfs()
-print(v[N-1][M-1])
+print(bfs(0, 0, N-1, M-1))
