2bcc7dd70e9ef9d41e28dd29ef595e1ffbbbbcc4
kimsuhyun <100765937+ksusuhyun@users.noreply.github.com>
2025-11-24 16:34:59 +0900

    [Silver I] Title: 미로 탐색, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split())
+arr = []
+for i in range(N):
+    row = list(map(int, input()))
+    arr.append(row)
+
+v = [[0]*M for i in range(N)]
+
+d = [(-1,0), (1,0), (0,-1), (0,1)]
+
+def check_in(x, y):
+    if 0<=x<N and 0<=y<M:
+        return True
+
+def bfs():
+    q = []
+    q.append((0,0))
+    v[0][0] = 1
+
+    while q:
+        c_x, c_y = q.pop(0)
+        for d_x, d_y in d:
+            new_x, new_y = c_x + d_x, c_y + d_y
+            if check_in(new_x, new_y) and arr[new_x][new_y] and not v[new_x][new_y]:
+                q.append((new_x, new_y))
+                v[new_x][new_y] = v[c_x][c_y] + 1
+
+bfs()
+print(v[N-1][M-1])
