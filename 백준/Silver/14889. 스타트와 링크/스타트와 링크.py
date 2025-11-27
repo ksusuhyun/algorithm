e2b9b7c285123612c45d059899a4c8c0b900dc90
kimsuhyun <100765937+ksusuhyun@users.noreply.github.com>
2025-11-27 15:57:02 +0900

    [Silver I] Title: 스타트와 링크, Time: 2112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,52 +1,36 @@
-import copy
-
 N = int(input())
 arr = []
 for _ in range(N):
     row = list(map(int, input().split()))
     arr.append(row)
 
-def cal_score(team):
-    score = 0
-    for i in team:
-        set_team = set(team)
-        set_i = set([i])
-        check_team = list(set_team - set_i)
-        for c in check_team:
-            score += arr[i-1][c-1]
-    
-    return score
+def cal(alst, blst):
 
-# 가능한 팀 조합 만들기
-total = []
-ans = []
-def dfs(s, team):
+    asm = 0
+    for i in alst:
+        for j in alst:
+            asm += arr[i][j]
 
-    ans.append(s)
+    bsm = 0
+    for i in blst:
+        for j in blst:
+            bsm += arr[i][j]
 
-    if len(ans) == team:
-        total.append(copy.deepcopy(ans))
-        ans.pop(-1)
+    return abs(asm-bsm)
+
+def dfs(n, alst, blst):
+    global ans
+
+    if n == N:
+        # 각 팀이 똑같은 인원으로 나눠지면
+        if len(alst) == len(blst):
+            ans = min(ans, cal(alst, blst))
         return
-
-    for i in range(s+1, N+1):
-        dfs(i, team)
     
-    team -= 1
-    ans.pop(-1)
+    dfs(n+1, alst+[n], blst)
+    dfs(n+1, alst, blst+[n])
+    
+ans = float('inf')
+dfs(0, [], [])
 
-dfs(1, N//2)
-
-# 반대편 팀 조합 만들기
-t = set([i for i in range(1, N+1)])
-total_b = []
-for a in total:
-    b = list(t - set(a))
-    total_b.append(b)
-
-# 점수 계산하기
-last = []
-for i in range(len(total)):
-    last.append(abs(cal_score(total[i]) - cal_score(total_b[i])))
-
-print(min(last))
+print(ans)
